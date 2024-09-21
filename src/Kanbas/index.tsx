e7135ce5d4287bd6bd16d34854d9f02d3ee25b84
maialeelemos <maia@lemosfamily.net>
2024-09-21 19:26:32 -0400

    Adds Courses screen (3.4) for A1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import Courses from "./Courses";
 
 export default function Kanbas() {
     return (
@@ -16,6 +17,9 @@ export default function Kanbas() {
                             <Route path="/" element={<Navigate to="Account" />} />
                             <Route path="/Account/*" element={<Account />} />
                             <Route path="/Dashboard" element={<Dashboard />} />
+                            <Route path="/Courses/:cid/*" element={<Courses />} />
+                            <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                            <Route path="/Inbox" element={<h1>Inbox</h1>} />
                         </Routes>
                     </td>
                 </tr>
