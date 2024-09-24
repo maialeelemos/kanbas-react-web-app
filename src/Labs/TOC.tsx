65e33d642653618c429572f5abea2ade4322220e
maialeelemos <maia@lemosfamily.net>
2024-09-24 09:30:30 -0400

    Adds github link and name to A1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <li><Link id="wd-github" to="https://github.com/maialeelemos/kanbas-react-web-app/tree/a1">Github</Link></li>
         </ul>
     );
 }
