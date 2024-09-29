9bb8c46114fd93a26d66591aba5593a659aceb24
maialeelemos <maia@lemosfamily.net>
2024-09-29 16:32:30 -0400

    Adds React Icons for Lab 2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -10,6 +10,7 @@ import "./index.css";
 import Margins from "./Margins";
 import Padding from "./Padding";
 import Positions from "./Positions";
+import ReactIconsSampler from "./ReactIcons";
 import Zindex from "./Zindex";
 export default function Lab2() {
   return (
@@ -84,6 +85,7 @@ export default function Lab2() {
       <Float />
       <GridLayout />
       <Flex />
+      <ReactIconsSampler />
       
     </div>
   );
