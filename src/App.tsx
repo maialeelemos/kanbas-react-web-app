08d50589f11bd8a38a3c6f02bbf2f81e2e8272b3
maialeelemos <maia@lemosfamily.net>
2024-11-03 11:23:51 -0500

    Adds edit, deleter, create for Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Labs from './Labs';
-import Kanbas from './Kanbas';
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import React from "react";
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Kanbas/store";
 
 function App() {
   return (
     <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kanbas/*" element={<Kanbas />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="Labs" />} />
+            <Route path="/Labs/*" element={<Labs />} />
+            <Route path="/Kanbas/*" element={<Kanbas />} />
+          </Routes>
+        </div>
+      </Provider>
     </HashRouter>
   );
 }
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -3,14 +3,25 @@ import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
-import * as db from "../../Database";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const [moduleName, setModuleName] = useState("");
+  const dispatch = useDispatch();
   return (
     <div>
-      <ModulesControls />
+      <ModulesControls
+        setModuleName={setModuleName}
+        moduleName={moduleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
+      />
       <br />
       <br />
       <br />
@@ -22,8 +33,32 @@ export default function Modules() {
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
-                {module.name}
-                <ModuleControlButtons />
+                {!module.editing && module.name}
+                {module.editing && (
+                  <input
+                    className="form-control w-50 d-inline-block"
+                    onChange={(e) =>
+                      dispatch(
+                        updateModule({ ...module, name: e.target.value })
+                      )
+                    }
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        dispatch(updateModule({ ...module, editing: false }));
+                      }
+                    }}
+                    defaultValue={module.name}
+                  />
+                )}
+                <ModuleControlButtons
+                  moduleId={module._id}
+                  deleteModule={(moduleId) => {
+                    dispatch(deleteModule(moduleId));
+                  }}
+                  editModule={(moduleId) => {
+                    dispatch(editModule(moduleId));
+                  }}
+                />
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
