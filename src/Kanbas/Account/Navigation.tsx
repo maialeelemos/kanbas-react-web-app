41b01a5263ea5dd549a4b6938422f53c1881636f
maialeelemos <maia@lemosfamily.net>
2024-11-29 11:07:00 -0500

    Adds up to section 4 of a6

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -4,21 +4,30 @@ import { Link, useLocation } from "react-router-dom";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   const { pathname } = useLocation();
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
         <Link
           to={`/Kanbas/Account/${link}`}
           className={`list-group-item border border-0 ${
-            pathname.includes(link)
-              ? "active"
-              : "text-danger"
+            pathname.includes(link) ? "active" : "text-danger"
           }`}
         >
           {link}
         </Link>
       ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kanbas/Account/Users`}
+          className={`list-group-item border border-0 ${active("Users")}`}
+        >
+          {" "}
+          Users{" "}
+        </Link>
+      )}
     </div>
   );
 }
